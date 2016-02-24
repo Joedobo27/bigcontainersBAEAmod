bd1cb0aa5b79df01a974c35ed25ff946b099a31d
Joe <joedobo123@gmail.com>
2016-02-24 11:35:50 -0800

    -+gems as bulk, changed try blocks, removed an implement causing a class to load and freeze out J-assist (unneeded anyway).

--- src/com/Joedobo27/WUmod/BigContainersBAEAMod.java
+++ src/com/Joedobo27/WUmod/BigContainersBAEAMod.java
@@ -14,7 +14,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 
-public class BigContainersBAEAMod implements WurmMod, Initable, ServerStartedListener, Configurable, ItemTypes {
+public class BigContainersBAEAMod implements WurmMod, Initable, ServerStartedListener, Configurable {
 
     private boolean makeContainersBig = false;
     private boolean removeOnePerTileLimits = false;
@@ -22,7 +22,7 @@ public class BigContainersBAEAMod implements WurmMod, Initable, ServerStartedLis
     private boolean makeItemsBulk = false;
     private boolean makeItemsCombine = false;
     private boolean removeInsideOutsideLimits = false;
-    private Logger logger = Logger.getLogger(this.getClass().getName());
+    private Logger logger = Logger.getLogger(BigContainersBAEAMod.class.getName());
 
     @Override
     public void configure(Properties properties) {
@@ -43,112 +43,164 @@ public class BigContainersBAEAMod implements WurmMod, Initable, ServerStartedLis
         int makeItemsCombineCount = 0;
         int removeInsideOutsideLimitsCount = 0;
 
+        //<editor-fold desc="Get Fields for reflection">
+        Map<Integer, ItemTemplate> fieldTemplates = null;
+        Field fieldOnePerTile = null;
+        Field fieldUsesSpecifiedContainerSizes = null;
+        Field fieldIsTransportable;
+        Field fieldCentimetersX = null;
+        Field fieldCentimetersY = null;
+        Field fieldCentimetersZ = null;
+        Field fieldWeight = null;
+        Field fieldBulk = null;
+        Field fieldCombine = null;
+        Field fieldOutsideonly = null;
+        Field fieldInsideOnly = null;
+        Field fieldUseOnGroundOnly;
+        Field fieldGem = null;
         try {
-            //<editor-fold desc="Get Fields for reflection">
-            Map<Integer, ItemTemplate> fieldTemplates = ReflectionUtil.getPrivateField(
+            fieldTemplates = ReflectionUtil.getPrivateField(
                         ItemTemplateFactory.class, ReflectionUtil.getField(ItemTemplateFactory.class, "templates"));
-            Field fieldOnePerTile = ReflectionUtil.getField(ItemTemplate.class, "onePerTile");
-            Field fieldUsesSpecifiedContainerSizes = ReflectionUtil.getField(ItemTemplate.class, "usesSpecifiedContainerSizes");
-            Field fieldIsTransportable = ReflectionUtil.getField(ItemTemplate.class, "isTransportable");
-            Field fieldCentimetersX = ReflectionUtil.getField(ItemTemplate.class, "centimetersX");
-            Field fieldCentimetersY = ReflectionUtil.getField(ItemTemplate.class, "centimetersY");
-            Field fieldCentimetersZ = ReflectionUtil.getField(ItemTemplate.class, "centimetersZ");
-            Field fieldWeight = ReflectionUtil.getField(ItemTemplate.class, "weight");
-            Field fieldBulk = ReflectionUtil.getField(ItemTemplate.class, "bulk");
-            Field fieldCombine = ReflectionUtil.getField(ItemTemplate.class, "combine");
-            Field fieldOutsideonly = ReflectionUtil.getField(ItemTemplate.class, "outsideonly");
-            Field fieldInsideOnly = ReflectionUtil.getField(ItemTemplate.class, "insideOnly");
-            Field fieldUseOnGroundOnly = ReflectionUtil.getField(ItemTemplate.class, "useOnGroundOnly");
-            //</editor-fold>
+            fieldOnePerTile = ReflectionUtil.getField(ItemTemplate.class, "onePerTile");
+            fieldUsesSpecifiedContainerSizes = ReflectionUtil.getField(ItemTemplate.class, "usesSpecifiedContainerSizes");
+            fieldIsTransportable = ReflectionUtil.getField(ItemTemplate.class, "isTransportable");
+            fieldCentimetersX = ReflectionUtil.getField(ItemTemplate.class, "centimetersX");
+            fieldCentimetersY = ReflectionUtil.getField(ItemTemplate.class, "centimetersY");
+            fieldCentimetersZ = ReflectionUtil.getField(ItemTemplate.class, "centimetersZ");
+            fieldWeight = ReflectionUtil.getField(ItemTemplate.class, "weight");
+            fieldBulk = ReflectionUtil.getField(ItemTemplate.class, "bulk");
+            fieldCombine = ReflectionUtil.getField(ItemTemplate.class, "combine");
+            fieldOutsideonly = ReflectionUtil.getField(ItemTemplate.class, "outsideonly");
+            fieldInsideOnly = ReflectionUtil.getField(ItemTemplate.class, "insideOnly");
+            fieldUseOnGroundOnly = ReflectionUtil.getField(ItemTemplate.class, "useOnGroundOnly");
+            fieldGem = ReflectionUtil.getField(ItemTemplate.class, "gem");
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+        //</editor-fold>
 
-            // ******************** start iteration through all the templates******************************
-            for (ItemTemplate template : fieldTemplates.values()) {
-                // *******************Set internal sizes big for select templates.*************************
-                Integer itemId = template.getTemplateId();
-                if (makeContainersBig) {
-                    if (template.isHollow() && (!template.isContainerLiquid() || itemId == ItemList.barrelHuge || itemId == ItemList.barrelLarge ||
-                            itemId == ItemList.stoneFountain || itemId == ItemList.stoneFountain2 || itemId == ItemList.amphoraLargePottery)) {
-                        if (!template.usesSpecifiedContainerSizes()) {
+        // ******************** start iteration through all the templates******************************
+        for (ItemTemplate template : fieldTemplates.values()) {
+            // *******************Set internal sizes big for select templates.*************************
+            Integer itemId = template.getTemplateId();
+            if (makeContainersBig) {
+                if (template.isHollow() && (!template.isContainerLiquid() || itemId == ItemList.barrelHuge || itemId == ItemList.barrelLarge ||
+                        itemId == ItemList.stoneFountain || itemId == ItemList.stoneFountain2 || itemId == ItemList.amphoraLargePottery)) {
+                    if (!template.usesSpecifiedContainerSizes()) {
+                        try {
                             ReflectionUtil.setPrivateField(template, fieldUsesSpecifiedContainerSizes, Boolean.TRUE);
+                        } catch (IllegalAccessException e) {
+                            e.printStackTrace();
                         }
-                        template.setContainerSize(1200, 1200, 1200);
-                        makeContainersBigCount++;
                     }
+                    template.setContainerSize(1200, 1200, 1200);
+                    makeContainersBigCount++;
                 }
-                //*********Remove one per tile restriction.*****************
-                if (removeOnePerTileLimits) {
+            }
+            //*********Remove one per tile restriction.*****************
+            if (removeOnePerTileLimits) {
 
-                    Boolean onePerTile;
+                Boolean onePerTile = null;
+                try {
                     onePerTile = ReflectionUtil.getPrivateField(template, fieldOnePerTile);
-                    if (onePerTile) {
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                if (onePerTile) {
+                    try {
                         ReflectionUtil.setPrivateField(template, fieldOnePerTile, Boolean.FALSE);
-                        removeOnePerTileLimitsCount++;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
+                    removeOnePerTileLimitsCount++;
                 }
-                /*
-                //***************Add Transportable flag to select templates**********************
-                if (itemId == STONE_ALTAR || itemId == TRASH_HEAP || itemId == ROWING_BOAT || itemId == WOOD_ALTAR
-                        || itemId == SILVER_ALTAR || itemId == GOLD_ALTAR) {
-                    if (!template.isTransportable()) {
-                        ReflectionUtil.setPrivateField(template, fieldIsTransportable, Boolean.TRUE);
-                    }
+            }
+            /*
+            //***************Add Transportable flag to select templates**********************
+            if (itemId == STONE_ALTAR || itemId == TRASH_HEAP || itemId == ROWING_BOAT || itemId == WOOD_ALTAR
+                    || itemId == SILVER_ALTAR || itemId == GOLD_ALTAR) {
+                if (!template.isTransportable()) {
+                    ReflectionUtil.setPrivateField(template, fieldIsTransportable, Boolean.TRUE);
                 }
-                */
-                //*****************redo size & weight for select templates**********************
-                if (resizePelt) {
-                    if (itemId == ItemList.pelt) {
+            }
+            */
+            //*****************redo size & weight for select templates**********************
+            if (resizePelt) {
+                if (itemId == ItemList.pelt) {
+                    try {
                         ReflectionUtil.setPrivateField(template, fieldCentimetersX, 10);
                         ReflectionUtil.setPrivateField(template, fieldCentimetersY, 10);
                         ReflectionUtil.setPrivateField(template, fieldCentimetersZ, 10);
                         ReflectionUtil.setPrivateField(template, fieldWeight, 100);
-                        logger.log(Level.INFO, "Pelt resized.");
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
+                    logger.log(Level.INFO, "Pelt resized.");
                 }
-                //****************add bulk flag to select templates***********************
-                if (makeItemsBulk) {
-                    if (itemId == ItemList.logHuge || itemId == ItemList.pelt || itemId == ItemList.saddle || itemId == ItemList.stoneKeystone ||
-                            itemId == ItemList.marbleKeystone || template.isFish() || itemId == ItemList.fishingHookWood ||
-                            itemId == ItemList.fishingHookIron || itemId == ItemList.fishingHookWoodAndString ||
-                            itemId == ItemList.fishingHookIronAndString) {
+            }
+            //****************add bulk flag to select templates***********************
+            if (makeItemsBulk) {
+                Boolean isGem = null;
+                try {
+                    isGem = ReflectionUtil.getPrivateField(template, fieldGem);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                if (itemId == ItemList.logHuge || itemId == ItemList.pelt || itemId == ItemList.saddle || itemId == ItemList.stoneKeystone ||
+                        itemId == ItemList.marbleKeystone || template.isFish() || itemId == ItemList.fishingHookWood ||
+                        itemId == ItemList.fishingHookIron || itemId == ItemList.fishingHookWoodAndString ||
+                        itemId == ItemList.fishingHookIronAndString || isGem) {
+                    try {
                         ReflectionUtil.setPrivateField(template, fieldBulk, Boolean.TRUE);
-                        makeItemsBulkCount++;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
+                    makeItemsBulkCount++;
                 }
-                //***************add combine flags to select templates*********************
-                if (makeItemsCombine) {
-                    if (itemId == ItemList.cochineal || itemId == ItemList.woad || itemId == ItemList.acorn) {
+            }
+            //***************add combine flags to select templates*********************
+            if (makeItemsCombine) {
+                if (itemId == ItemList.cochineal || itemId == ItemList.woad || itemId == ItemList.acorn) {
+                    try {
                         ReflectionUtil.setPrivateField(template, fieldCombine, Boolean.TRUE);
-                        makeItemsCombineCount++;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
+                    makeItemsCombineCount++;
                 }
-                //*****************remove outside and inside only flags from all**********************
-                if (removeInsideOutsideLimits) {
-                    Boolean insideOnly;
+            }
+            //*****************remove outside and inside only flags from all**********************
+            if (removeInsideOutsideLimits) {
+                Boolean insideOnly = null;
+                try {
                     insideOnly = ReflectionUtil.getPrivateField(template, fieldInsideOnly);
-                    if (template.isOutsideOnly() || insideOnly) {
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                }
+                if (template.isOutsideOnly() || insideOnly) {
+                    try {
                         ReflectionUtil.setPrivateField(template, fieldOutsideonly, Boolean.FALSE);
                         ReflectionUtil.setPrivateField(template, fieldInsideOnly, Boolean.FALSE);
-                        removeInsideOutsideLimitsCount++;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
                     }
+                    removeInsideOutsideLimitsCount++;
                 }
-                //*****************************remove ground only flags***************************
-                /*
-                if (itemId == BSB || itemId == FSB){
-                    if (template.isUseOnGroundOnly()) {
-                        ReflectionUtil.setPrivateField(template, fieldUseOnGroundOnly, Boolean.FALSE);
-                    }
-                }
-                */
             }
-            logger.log(Level.INFO, "Number of Containers maxed to 1,728,000 L is " + Integer.toString(makeContainersBigCount));
-            logger.log(Level.INFO, "Number of one per tile restrictions removed is " + Integer.toString(removeOnePerTileLimitsCount));
-            logger.log(Level.INFO, "Number of items marked as bulk is " + Integer.toString(makeItemsBulkCount));
-            logger.log(Level.INFO, "Number of items marked as combine is " + Integer.toString(makeItemsCombineCount));
-            logger.log(Level.INFO, "Number of inside or outside only flags removed is " + Integer.toString(removeInsideOutsideLimitsCount));
-
-        }catch (IllegalAccessException | NoSuchFieldException e){
-            logger.log(Level.WARNING, e.toString());
+            //*****************************remove ground only flags***************************
+            /*
+            if (itemId == BSB || itemId == FSB){
+                if (template.isUseOnGroundOnly()) {
+                    ReflectionUtil.setPrivateField(template, fieldUseOnGroundOnly, Boolean.FALSE);
+                }
+            }
+            */
         }
+        logger.log(Level.INFO, "Number of Containers maxed to 1,728,000 L is " + Integer.toString(makeContainersBigCount));
+        logger.log(Level.INFO, "Number of one per tile restrictions removed is " + Integer.toString(removeOnePerTileLimitsCount));
+        logger.log(Level.INFO, "Number of items marked as bulk is " + Integer.toString(makeItemsBulkCount));
+        logger.log(Level.INFO, "Number of items marked as combine is " + Integer.toString(makeItemsCombineCount));
+        logger.log(Level.INFO, "Number of inside or outside only flags removed is " + Integer.toString(removeInsideOutsideLimitsCount));
     }
 
     @Override
